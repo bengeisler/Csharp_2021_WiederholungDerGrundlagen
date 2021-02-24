be232a4a5df5d6cdef8aaf7e758e9b18a27e1c7f
Benedikt Geisler <benedikt.geisler@posteo.de>
2021-02-24 09:02:39 +0100

    Schleifen

--- Csharp_2021_WiederholungDerGrundlagen/Form1.Designer.cs
+++ Csharp_2021_WiederholungDerGrundlagen/Form1.Designer.cs
@@ -30,6 +30,7 @@ namespace Csharp_2021_WiederholungDerGrundlagen
 		private void InitializeComponent()
 		{
 			this.btnOperatoren = new System.Windows.Forms.Button();
+			this.btnSchleifen = new System.Windows.Forms.Button();
 			this.SuspendLayout();
 			// 
 			// btnOperatoren
@@ -42,11 +43,22 @@ namespace Csharp_2021_WiederholungDerGrundlagen
 			this.btnOperatoren.UseVisualStyleBackColor = true;
 			this.btnOperatoren.Click += new System.EventHandler(this.btnOperatoren_Click);
 			// 
+			// btnSchleifen
+			// 
+			this.btnSchleifen.Location = new System.Drawing.Point(12, 41);
+			this.btnSchleifen.Name = "btnSchleifen";
+			this.btnSchleifen.Size = new System.Drawing.Size(75, 23);
+			this.btnSchleifen.TabIndex = 1;
+			this.btnSchleifen.Text = "Schleifen";
+			this.btnSchleifen.UseVisualStyleBackColor = true;
+			this.btnSchleifen.Click += new System.EventHandler(this.btnSchleifen_Click);
+			// 
 			// Form1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.btnSchleifen);
 			this.Controls.Add(this.btnOperatoren);
 			this.Name = "Form1";
 			this.Text = "Wiederholung der Grundlagen";
@@ -57,6 +69,7 @@ namespace Csharp_2021_WiederholungDerGrundlagen
 		#endregion
 
 		private System.Windows.Forms.Button btnOperatoren;
+		private System.Windows.Forms.Button btnSchleifen;
 	}
 }
 
--- Csharp_2021_WiederholungDerGrundlagen/Form1.cs
+++ Csharp_2021_WiederholungDerGrundlagen/Form1.cs
@@ -53,7 +53,35 @@ namespace Csharp_2021_WiederholungDerGrundlagen
 			// 8.
 			k += " ist kurz.";    // Text anhängen
 			Console.WriteLine(k);
+		}
 
+		private void btnSchleifen_Click(object sender, EventArgs e)
+		{
+			// 1.a
+			Console.WriteLine("1a");
+			// Start bei 1, damit die Nummer des Durchlaufs auch korrekt bei 1 beginnt
+			for (int i = 1; i < 6; i++)		// Code-Snippen für for-Schleife: for
+			{
+				Console.WriteLine("Durchlauf: " + i);
+			}
+
+			// 1.b
+			Console.WriteLine("1b");
+			for (int i = 5; i < 10; i++)
+			{
+				Console.WriteLine("Durchlauf: " + i);
+			}
+
+			// 1.c
+			Console.WriteLine("1c");
+			// Diese Schleife wird 6 mal ausgeführt, da der Schleifenzähler bei 0
+			// startet und bis einschließlich 5 läuft
+			int durchlaufZähler = 0;
+			for (int i = 0; i <= 5; i++)
+			{
+				durchlaufZähler++;
+			}
+			Console.WriteLine($"Diese Schleife wurde {durchlaufZähler} mal durchlaufen");
 		}
 	}
 }
