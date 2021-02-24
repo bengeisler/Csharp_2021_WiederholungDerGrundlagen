65fa6d56386f27c9828e9b5027f37fb6acf19dfa
Benedikt Geisler <benedikt.geisler@posteo.de>
2021-02-24 08:53:14 +0100

    Projektdateien hinzufügen.

---
+++ Csharp_2021_WiederholungDerGrundlagen/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿
+namespace Csharp_2021_WiederholungDerGrundlagen
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Erforderliche Designervariable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Verwendete Ressourcen bereinigen.
+		/// </summary>
+		/// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Vom Windows Form-Designer generierter Code
+
+		/// <summary>
+		/// Erforderliche Methode für die Designerunterstützung.
+		/// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.btnOperatoren = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// btnOperatoren
+			// 
+			this.btnOperatoren.Location = new System.Drawing.Point(12, 12);
+			this.btnOperatoren.Name = "btnOperatoren";
+			this.btnOperatoren.Size = new System.Drawing.Size(75, 23);
+			this.btnOperatoren.TabIndex = 0;
+			this.btnOperatoren.Text = "Operatoren";
+			this.btnOperatoren.UseVisualStyleBackColor = true;
+			this.btnOperatoren.Click += new System.EventHandler(this.btnOperatoren_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.btnOperatoren);
+			this.Name = "Form1";
+			this.Text = "Wiederholung der Grundlagen";
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button btnOperatoren;
+	}
+}
+
---
+++ Csharp_2021_WiederholungDerGrundlagen/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Csharp_2021_WiederholungDerGrundlagen
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		// Aufgabe "Operatoren"
+		private void btnOperatoren_Click(object sender, EventArgs e)
+		{
+			// 1.
+			int i = 0;						// Lokale Variable vom Datentyp "int" erstellen
+			i++;									// ++ Operator: Variable um 1 erhöhen. 
+			Console.WriteLine(i); // Variable in der Konsole ausgeben. Kürzel für Code-Snippet: cw
+
+			// 2.
+			i += 3;               // Variable um 3 erhöhen. Alternativschreibweise: i = i + 3;
+			Console.WriteLine(i);
+
+			// 3.
+			i *= 3;               // Variable mit 3 multiplizieren. Alternativschreibweise: i = i * 3;
+			Console.WriteLine(i);
+
+			// 4.
+			double erhöhungInProzent = i * 0.2;		// Erhöhung um 20% berechnen (Datentyp double)
+			i += (int)erhöhungInProzent;					// Diese Erhöhung zu i hinzuzählen, dabei double in int wandeln
+			Console.WriteLine(i);						
+
+			// 5.
+			int ergebnis = i / 4;		// Ergebnis der Division berechnen
+			int rest = i % 4;				// Mit dem Modulo-Operator den Rest der Division berechnen
+			Console.WriteLine($"Ergebnis: {ergebnis}, Rest: {rest}");
+
+			// 6.
+			Console.WriteLine("Der aktuelle Wert der Zahl beträgt: " + ergebnis);
+
+			// 7.
+			string k = "Mein Text";
+			Console.WriteLine(k);
+
+			// 8.
+			k += " ist kurz.";    // Text anhängen
+			Console.WriteLine(k);
+
+		}
+	}
+}
